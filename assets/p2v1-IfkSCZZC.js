c8bcb2bab8a3ec8fc5bcf553892b5805af421990
enmourn <enmourn@gmail.com>
2025-02-28 19:01:13 +0300

    deploy

---
+++ assets/p2v1-IfkSCZZC.js
@@ -0,0 +1,1 @@
+const r={columns:12,rows:12},t=[{column:{start:1,end:6},row:{start:1,end:12}},{column:{start:7,end:12},row:{start:1,end:12}}],s={grid:r,gridAreas:t};export{s as default,r as grid,t as gridAreas};
